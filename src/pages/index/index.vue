6ad6dabcc1e178c13cc2d34b7af202c332c9d778
liwenfeng <ananas.leee@gmail.com>
2022-08-29 12:47:05 +0800

    fix: fix

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,19 +1,33 @@
 <template>
+  <view w-full h18 class="hover:(bg-gray-400 font-black) font-(light mono)">hover:(bg-gray-400 font-black)</view>
+
+  <view w-full h18 bg-right bg-no-repeat bg-contain class="bg-[url(static/logo.png)]">class="bg-[url(static/logo.png)]"
+  </view>
+
+  <view w-full h18 bg-right bg-no-repeat bg-contain class="bg-[url(/static/logo.png)]">
+    class="bg-[url(/static/logo.png)]"</view>
+
+  <view w-full h18 bg-right bg-no-repeat bg-contain style="background-image: url('/static/logo.png');">test bg</view>
+
+  <view w-full h18 bg-right bg-no-repeat bg-contain
+    class="bg-[url(https://raw.githubusercontent.com/unocss/unocss/main/playground/public/icon-gray.svg)]">
+    class="bg-[url(https://raw.githubusercontent.com/unocss/unocss/main/playground/public/icon-gray.svg)]"</view>
+
+  <view w-full h18 class="bg-red">class="bg-red"</view>
+
+  <view w-full h18 bg-green>bg-green</view>
+
+  <view w-full h18 class="bg-#F2FF2F">class="bg-#F2FF2F"</view>
 
 
-  <view w8 h8 class="hover:(bg-gray-400 font-medium) font-(light mono)">test</view>
-  <view w18 h18 class="bg-[url(static/logo.png)]">test</view>
-  <view w18 h18 class="bg-[url(/static/logo.png)]">test</view>
-  <view w18 h18 style="background-image: url('/static/logo.png');">test</view>
+  <view w-full h18 bg="green">bg="green"</view>
 
-  <view w28 h28 bg-center class="bg-[url(https://raw.githubusercontent.com/unocss/unocss/main/playground/public/icon-gray.svg)]">
-    test</view>
-  <view w8 h8 class="bg-red">test</view>
-  <view w8 h8 bg-green>test</view>
-  <view w8 h8 class="bg-#F2FF2F">test</view>
-  <view w8 h8 class="bg-[#F2FF2F]">test</view>
-  <view w8 h8 text="base #F2FF" font="serif">test</view>
-  <view text-b-f>test</view>
+
+  <view w-full h18 class="bg-[#F2FF2F]">class="bg-[#F2FF2F]"</view>
+
+  <view w-full h18 text="base #F2FF" font="serif">text="base #F2FF" font="serif"</view>
+
+  <view w-full text-b-f text-base text-black>text-b-f text-base text-black</view>
 
   <uni-easyinput v-model="form.phone" :input-border="false" :trim="true" type="number" prefix-icon="person"
     placeholder="请输入手机号码" />
@@ -39,56 +53,13 @@
 
 <script setup lang="ts">
 import { reactive, ref } from 'vue'
-const title = ref('Hello')
 
 const form = reactive({
   phone: '',
 })
 
-async function logout() {
-  uni.showLoading({ title: '退出登录中...' })
 
-  // await apiWitUserLogout()
-
-  uni.hideLoading()
-
-  uni.showToast({
-    title: '已退出',
-    icon: 'success',
-    duration: 800,
-    success() {
-      setTimeout(() => {
-        uni.navigateTo({ url: '/pages/login/login' })
-      }, 800)
-    },
-  })
-}
 </script>
 
 <style>
-.content {
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-}
-
-.logo {
-  height: 200rpx;
-  width: 200rpx;
-  margin-top: 200rpx;
-  margin-left: auto;
-  margin-right: auto;
-  margin-bottom: 50rpx;
-}
-
-.text-area {
-  display: flex;
-  justify-content: center;
-}
-
-.title {
-  font-size: 36rpx;
-  color: #8f8f94;
-}
 </style>
--- unocss.config.ts
+++ unocss.config.ts
@@ -1,3 +1,5 @@
+import type { Preset, SourceCodeTransformer } from 'unocss'
+
 import {
   defineConfig,
   presetAttributify,
@@ -9,61 +11,78 @@ import {
   transformerVariantGroup,
 } from 'unocss'
 
-import { presetApplet, presetRemToRpx, transformerRenameClass } from 'unocss-applet'
+import {
+  presetApplet,
+  presetRemToRpx,
+  transformerApplet,
+  transformerAttributify,
+} from 'unocss-applet'
 
-// https://github.com/unocss/unocss/tree/main/packages/preset-attributify/
-const presets: any = [presetAttributify()]
+const presets: Preset[] = []
 
-// https://github.com/unocss/unocss/tree/main/packages/transformer-directives
-const transformers: any = [transformerDirectives()]
-
-console.log('process.env.UNI_PLATFORM', process.env.UNI_PLATFORM)
-
+const transformers: SourceCodeTransformer[] = []
 
 if (process.env.UNI_PLATFORM === 'h5') {
   // https://github.com/unocss/unocss/tree/main/packages/preset-uno
   presets.push(presetUno())
 
-  // https://github.com/unocss/unocss/tree/main/packages/transformer-variant-group
-  transformers.push(transformerVariantGroup())
-} else {
+  presets.push(presetAttributify())
+}
+else {
   // https://github.com/unocss-applet/unocss-applet
   presets.push(presetApplet())
   presets.push(presetRemToRpx())
 
-  transformers.push(transformerRenameClass())
+  // don't change the order
+  // https://github.com/unocss/unocss/tree/main/packages/preset-attributify/
+  transformers.push(transformerAttributify())
+  transformers.push(transformerApplet())
 }
 
-// presets.push(presetAttributify())
-
-// https://github.com/unocss/unocss/tree/main/packages/preset-icons
-presets.push(presetIcons({
-  scale: 1.2,
-  warn: true,
-}))
-
-// https://github.com/unocss/unocss/tree/main/packages/preset-typography
-presets.push(presetTypography())
-
-// https://github.com/unocss/unocss/tree/main/packages/preset-web-fonts
-presets.push(presetWebFonts({
-  fonts: {
-    sans: 'DM Sans',
-    serif: 'DM Serif Display',
-    mono: 'DM Mono',
-  },
-}))
-
-
 export default defineConfig({
+  // theme: {
+  //   backgroundSize: {
+  //     auto: 'auto',
+  //     cover: 'cover',
+  //     contain: 'contain',
+  //   },
+  // },
   shortcuts: [
     ['btn', 'px-4 py-1 rounded inline-block bg-teal-700 text-white cursor-pointer hover:bg-teal-800 disabled:cursor-default disabled:bg-gray-600 disabled:opacity-50'],
     ['icon-btn', 'inline-block cursor-pointer select-none opacity-75 transition duration-200 ease-in-out hover:opacity-100 hover:text-teal-600'],
-    ['text-b-f', 'text-base text-#F2FFCC'],
+    ['text-b-f', 'text-base text-#801DAE font-sans text-4xl'],
+    ['flex-center', 'flex items-center'],
+    ['flex-col-center', 'flex flex-col items-center'],
   ],
 
-  presets,
-  transformers,
+  presets: [
+    presetIcons({
+      warn: true,
+    }),
+
+    // https://github.com/unocss/unocss/tree/main/packages/preset-typography
+    presetTypography(),
+
+    // https://github.com/unocss/unocss/tree/main/packages/preset-web-fonts
+    presetWebFonts({
+      fonts: {
+        sans: 'DM Sans',
+        serif: 'DM Serif Display',
+        mono: 'DM Mono',
+      },
+    }),
+
+    ...presets,
+  ],
+  transformers: [
+    // https://github.com/unocss/unocss/tree/main/packages/transformer-directives
+    transformerDirectives(),
+
+    // https://github.com/unocss/unocss/tree/main/packages/transformer-variant-group
+    transformerVariantGroup(),
+
+    ...transformers,
+  ],
   // https://github.com/unocss/unocss#safelist
   safelist: 'prose prose-sm m-auto text-left'.split(' '),
 })
